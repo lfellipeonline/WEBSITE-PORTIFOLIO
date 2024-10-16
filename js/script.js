268d33c00c13143e29ddc9eedc5671a0acf325c3
Luiz Felipe Rodrigues de Melo <mail.lfellipe@gmail.com>
2024-10-16 15:22:49 -0300

    commit 16/10 experimental change viewport

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
- function homebarConfigClick() {
+// Funções da Aba Flutuante
+function homebarConfigClick() {
      let abaFlutuante = document.getElementById('abaFlutuante');
      if (abaFlutuante.style.display === 'none' || abaFlutuante.style.display === '') {
          abaFlutuante.style.display = 'block';
@@ -7,8 +8,42 @@
      }
  }
 
+// Funções do Alternar Visualização da Aba Flutuante
+document.getElementById('alternarVisualização').onclick = function() {
+    if (document.getElementById('buttonExplore').style.backgroundColor === 'black') {
+        document.getElementById('buttonExplore').style.backgroundColor = 'white';
+        document.getElementById('buttonExplore').style.color = 'black';
+
+        document.getElementById('sobreBackgroundContainer').style.backgroundColor = 'white';
+        document.getElementById('sobreMain').style.color = 'black';
+        document.getElementById('sobreSub').style.color = 'black';
+        document.getElementById('sobreBackgroundContainer').style.boxShadow = '0px 0px 10px 15px white';
+
+        document.getElementById('contatoContainer').style.backgroundColor = 'white';
+        document.getElementById('contatoTitle').style.color = 'black';
+    } else {
+        document.getElementById('buttonExplore').style.backgroundColor = 'black';
+        document.getElementById('buttonExplore').style.color = 'white';
+
+        document.getElementById('sobreBackgroundContainer').style.backgroundColor = 'black';
+        document.getElementById('sobreMain').style.color = 'white';
+        document.getElementById('sobreSub').style.color = 'white';
+        document.getElementById('sobreBackgroundContainer').style.boxShadow = '0px 0px 10px 15px black';
+
+        document.getElementById('contatoContainer').style.backgroundColor = 'black';
+        document.getElementById('contatoTitle').style.color = 'white';
+    }
+}
+
+
+// Funções do Button Explore
 document.getElementById('buttonExplore').onclick = function() {
     location.href = '#sobreMain'
 }
 
+
+
+
+
+// Debug Console
 console.log('Script carregado com sucesso!');
