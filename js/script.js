382a780ee7ddd8a85d31da03a91db90e399f9f99
Luiz Felipe Rodrigues de Melo <mail.lfellipe@gmail.com>
2024-10-16 14:42:18 -0300

    commit 16/10 style change

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
-function homebarConfigClick() {
-    let abaFlutuante = document.getElementById('abaFlutuante');
-    if (abaFlutuante.style.display === 'none' || abaFlutuante.style.display === '') {
-        abaFlutuante.style.display = 'block';
-    } else {
-        abaFlutuante.style.display = 'none';
-    }
-}
+ function homebarConfigClick() {
+     let abaFlutuante = document.getElementById('abaFlutuante');
+     if (abaFlutuante.style.display === 'none' || abaFlutuante.style.display === '') {
+         abaFlutuante.style.display = 'block';
+     } else {
+         abaFlutuante.style.display = 'none';
+     }
+ }
 
 document.getElementById('buttonExplore').onclick = function() {
     location.href = '#sobreMain'
